20ce306af88f8f22e1f56b698862690978b117ad
BIT-USER <BIT-USER@BIT>
2016-09-06 17:38:29 +0900

    1st

--- src/practice01/MultipleOfTree.java
+++ src/practice01/MultipleOfTree.java
@@ -6,6 +6,17 @@ public class MultipleOfTree {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
+		System.out.print("수를 입력하세요 : ");
+		
+		int tree = scanner.nextInt();
+		if( tree % 3 == 0)	{
+			System.out.println("");
+			System.out.println("3의 배수입니다 :)");
+		} else	{
+			System.out.println("");
+			System.out.println("3의 배수가 아닙니다 X(");
+		}
+		
 		scanner.close();
 	}
 }
--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -4,6 +4,10 @@ package practice01;
 public class Prob2 {
 
 	public static void main(String[] args) {
+		for (int i=0; i<=8; i++)	{
+			System.out.println(i);
+					
+		}
 	}
 
 }
